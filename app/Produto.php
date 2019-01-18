3b16fd9c3ae4cdf509601536af5cfb71a2e7f1df
Guilherme <soad_guilherme@hotmail.com>
2019-01-18 12:14:35 -0200

    Update Produto.php
    Adição de comentários

--- app/Produto.php
+++ app/Produto.php
@@ -4,6 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+* Lista dos campos do produto
+* Obs.: o Campo UnidadeVenda possui opções
+* ('Caixa', 'Unitário', 'Pacote', 'Saco')
+*/
+
 class Produto extends Model
 {
     protected $fillable = ['sku', 'nome', 'valorUnitario', 'UnidadeVenda', 'created_at', 'updated_at'];
